90325cbea99dcb0aa9e931067242bf5694a07141
Ahtziri <Afalcon1209@gmail.com>
2022-03-11 11:35:24 -0800

    add Loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,12 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+import Loader from '../components/Loader'
+
 export default function Home() {
   return (
     <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'ahtzi' }
-      }}>
-      <a>Ahtzi's Profile</a>
-      </Link>
+      <Loader show/>
     </div>
   )
 }
